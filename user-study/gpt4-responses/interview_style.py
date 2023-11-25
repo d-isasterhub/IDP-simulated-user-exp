15c1f957c14423b6568f273c0f2928e043bf4127
d-isasterhub <64603343+d-isasterhub@users.noreply.github.com>
2023-11-25 22:32:27 +0100

    created function for creating prompts

--- user-study/gpt4-responses/interview_style.py
+++ user-study/gpt4-responses/interview_style.py
@@ -2,20 +2,24 @@ import interview_util_prompts
 import interview_util
 import openai
 import os
+import pandas as pd
 
 # df['output_2'] = (df['input_1'] + df['input_2'])
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def get_response(row, colnames:list, question:str):
+def get_response(row):
     messages = interview_util.MESSAGE_BASE
 
-    response = openai.ChatCompletion.create(
-        model = "gpt-4-vision-preview",
-        max_tokens = 300,
-        messages = messages
-    )
-    pass
+# def get_response(row, colnames:list, question:str):
+#     messages = interview_util.MESSAGE_BASE
+
+#     response = openai.ChatCompletion.create(
+#         model = "gpt-4-vision-preview",
+#         max_tokens = 300,
+#         messages = messages
+#     )
+#     pass
 
 # print(response)
 # print(response["choices"][0]["message"]["content"])
--- user-study/gpt4-responses/interview_util_images.py
+++ user-study/gpt4-responses/interview_util_images.py
@@ -35,3 +35,198 @@ WARM_UP_TASK_1_RHINOCEROS_IMAGES = [
         "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/rhinocerosauklet_04.png?raw=true"
     }
 ]
+
+WARM_UP_TASK_2_LEAST_IMAGES = [
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/leastauklet_05.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/leastauklet_06.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/leastauklet_07.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/leastauklet_08.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/leastauklet_09.png?raw=true"
+    }
+]
+
+WARM_UP_TASK_2_RHINOCEROS_IMAGES = [
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/rhinocerosauklet_05.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/rhinocerosauklet_06.png?raw=true"
+    },
+]
+
+WARM_UP_TASK_2_CRESTED_IMAGES = [
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/crestedauklet_01.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/crestedauklet_02.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/crestedauklet_03.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/crestedauklet_04.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/crestedauklet_05.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/crestedauklet_06.png?raw=true"
+    },
+]
+
+WARM_UP_TASK_2_PARAKEET_IMAGES = [
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/parakeetauklet_01.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/parakeetauklet_02.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/parakeetauklet_03.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/parakeetauklet_04.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/parakeetauklet_05.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/parakeetauklet_06.png?raw=true"
+    },
+    {
+        "type": "image_url",
+        "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/parakeetauklet_07.png?raw=true"
+    },
+]
+
+IMAGE_MAPPING_AUKLET = {
+    'Q1': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/0-Crested.png?raw=true"
+        }],
+    'Q2': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/1-Crested.png?raw=true"
+        }],
+    'Q3': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/2-Crested.png?raw=true"
+        }],
+    'Q4':  
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/3-Crested.png?raw=true"
+        }],
+    'Q5': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/4-Crested.png?raw=true"
+        }],
+    'Q6': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/15-Least.png?raw=true"
+        }],
+    'Q7': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/16-Least.png?raw=true"
+        }],
+    'Q8':
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/17-Least.png?raw=true"
+        }],
+    'Q9': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/18-Least.png?raw=true"
+        }],
+    'Q10': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/19-Least.png?raw=true"
+        }],
+    'Q11': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/31-Parakeet.png?raw=true"
+        }],
+    'Q12':  
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/32-Parakeet.png?raw=true"
+        }],
+    'Q13':  
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/37-Parakeet.png?raw=true"
+        }],
+    'Q14':  
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/38-Parakeet.png?raw=true"
+        }],
+    'Q15':  
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/47-Parakeet.png?raw=true"
+        }],
+    'Q16': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/50-Rhinoceros.png?raw=true"
+        }],
+    'Q17': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/51-Rhinoceros.png?raw=true"
+        }],
+    'Q18': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/53-Rhinoceros.png?raw=true"
+        }],
+    'Q19': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/54-Rhinoceros.png?raw=true"
+        }],
+    'Q20': 
+        [{
+            "type": "image_url",
+            "image_url": "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/55-Rhinoceros.png?raw=true"
+        }],
+}
