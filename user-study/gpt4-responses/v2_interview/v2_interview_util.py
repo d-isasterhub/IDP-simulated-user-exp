bbc4719add298e3e002d8ca94a00e1b2e65fbe7b
d-isasterhub <60436449+IsabelSchorr@users.noreply.github.com>
2023-11-28 19:53:19 +0100

    updated comments and role checking

--- user-study/gpt4-responses/v2_interview/v2_interview_util.py
+++ user-study/gpt4-responses/v2_interview/v2_interview_util.py
@@ -3,7 +3,19 @@
 
 # Utils
 def get_msg(role:str, prompt:str):
-    # allows you to simply concatenate messages (messages = msg1 + msg2)
+    ''' Creates a message that can directly be used for an OpenAI api call: 
+    messages = get_msg(role, prompt)
+
+    To concatenate messages, write the following (example):
+    messages = get_msg(role1, prompt1) + get_msg(role2, prompt2)
+    
+    Args:
+        role (str) : a valid role (system, user, assistant)
+        prompt (str) : the content of the message
+
+    Returns:
+        msg (list) : the message in a format directly usable for OpenAI api calls
+    '''
     _role_check(role)
     msg = [
         {"role": role, "content": prompt}
@@ -11,7 +23,22 @@ def get_msg(role:str, prompt:str):
     return msg
 
 def get_msg_with_image(role:str, prompt:str, image:str):
-    # image example: "9-Crested.png", maybe modify to take links instead
+    ''' Creates a message that can directly be used for an OpenAI api call and contains one
+    image: 
+    messages = get_msg_with_image(role, prompt, image)
+
+    To concatenate the generated message with other messages, write the following (example):
+    messages = get_msg(role1, prompt1) + get_msg(role2, prompt2, image2)
+    
+    Args:
+        role (str) : a valid role (system, user, assistant)
+        prompt (str) : the content of the message
+        image (str) : the name of the image (e.g. "0-Crested.png"), the image must be in the 
+                      images folder and uploaded to github
+
+    Returns:
+        msg (list) : the message in a format directly usable for OpenAI api calls
+    '''
     _role_check(role)
     img_url = "https://github.com/d-isasterhub/IDP-simulated-user-exp/blob/main/images/" + image + "?raw=true"
     msg = [{"role": role, "content": [
@@ -28,5 +55,15 @@ def get_msg_with_image(role:str, prompt:str, image:str):
     
 
 def _role_check(role:str):
-    # TODO: system, user, assistant
-    pass
+    ''' Checks whether the given role is valid. Throws an error if the role is not valid.
+    Accepted roles:
+    - system
+    - user
+    - assistant
+    
+    Args:
+        role (str) : the role to be tested
+    '''
+    roles = ['system', 'user', 'assistant']
+    if role not in roles:
+        raise ValueError("Invalid role type. Expected one of: %s" % roles)
