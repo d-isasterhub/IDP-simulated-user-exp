1d5b175cedcf7d90470da59425bfce2ad95a0593
d-isasterhub <isabels.email@yahoo.de>
2023-12-03 13:57:53 +0100

    created layout for tests

---
+++ user-study/gpt4-responses/v2_interview/tests/test_v2_interview_util.py
@@ -0,0 +1,20 @@
+
+import sys
+# setting path
+sys.path.append('../v2_interview')
+
+import pytest
+
+from v2_interview_util import (
+    get_msg
+)
+
+# Current folder: IDP-simulated-user-exp\user-study\gpt4-responses\v2_interview
+# Execute: pytest tests 
+
+def test_test():
+    assert True
+
+# @pytest.mark.xfail
+def test_test_2():
+    assert False
