b566aa85ee12e8abf26e7af17482a335e9ef2f5c
Rai T <53047500+bowl-of-rais@users.noreply.github.com>
2023-12-07 19:46:36 +0100

    Delete casual_test.py

--- user-study/gpt4-responses/v2_interview/casual_test.py
+++
@@ -1,8 +0,0 @@
-from v2_interview import read_human_data, create_userprofiles
-
-
-data = read_human_data("user-study/data-exploration-cleanup/cleaned_simulatedusers.csv")
-profiles = create_userprofiles(data)
-
-for p in profiles:
-    print(p)
